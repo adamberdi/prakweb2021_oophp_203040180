d79dae75e4c42968c58337bd29ff94c5a8c01555
adamberdi <adamberdi46@gmail.com>
2022-01-11 13:20:41 +0700

    Menyelesaikan Video 2 - Class & Object

---
+++ pertemuan1/coba.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Adam Berdi R. Adipati
+203040180
+https://github.com/adamberdi/prakweb2021_oophp_203040180
+Pertemuan 2 - 11 Januari 2022
+Mempelajari class pada PHP
+*/
+?>
+
+<?php
+
+class Coba
+{
+}
+
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
