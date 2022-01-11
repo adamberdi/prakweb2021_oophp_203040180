48c91be7331b2bfd7d8267f82543927325bdde35
adamberdi <adamberdi46@gmail.com>
2022-01-11 13:25:08 +0700

    Menyelesaikan Video 2 - Class & Object

---
+++ pertemuan1/produk.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Adam Berdi RA
+203040180
+https://github.com/adamberdi/prakweb2021_oophp_203040180
+Pertemuan 2 - 11 Januari 2022
+Mempelajari object pada PHP
+*/
+?>
+<?php
+
+// Jualan Produk 
+// Komik
+// Game
+
+class Produk
+{
+}
+
+$produk1 = new Produk();
+$produk2 = new Produk();
